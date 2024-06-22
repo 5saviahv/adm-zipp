729f517b075f1a72b2eef46c723e7593603ef02b
5saviahv <5saviahv@users.noreply.github.com>
2024-06-23 01:50:08 +0300

    zip64ExtraInfo

--- headers/extraHeader.js
+++ headers/extraHeader.js
@@ -91,7 +91,7 @@ ExtraHeader.prototype.generateBuffer = function () {
  * @returns {object} - zip64 header values
  *
  */
-ExtraHeader.prototype.GetZip64 = function () {
+ExtraHeader.prototype.GetZip64 = function (centralHeader) {
     const zip64 = {};
     const big = (val) => (typeof val === "bigint" ? val : BigInt(val || 0));
     if (this.has(Constants.ID_ZIP64)) {
@@ -109,40 +109,60 @@ ExtraHeader.prototype.GetZip64 = function () {
             zip64.diskNumStart = toNumber(data.readUInt32LE(ID_ZIP64_OFFSETS.DISKNUM));
         }
     }
+    if (typeof centralHeader == "object") {
+        if ("size" in zip64 && centralHeader.size === Constants.EF_ZIP64_OR_32) {
+            centralHeader.size = zip64.size;
+        }
+        if ("compressedSize" in zip64 && centralHeader.compressedSize === Constants.EF_ZIP64_OR_32) {
+            centralHeader.compressedSize = zip64.compressedSize;
+        }
+        if ("offset" in zip64 && centralHeader.offset === Constants.EF_ZIP64_OR_32) {
+            centralHeader.offset = zip64.offset;
+        }
+        if ("diskNumStart" in zip64 && centralHeader.diskNumStart === Constants.EF_ZIP64_OR_16) {
+            centralHeader.diskNumStart = zip64.diskNumStart;
+        }
+    }
+
     return zip64;
 };
 
 /**
  * Set Zip64 extra header values
  *
- * @param {object} zip64 - zip64 header values
+ * @param {object} centralHeader - zip64 header values
  * @param {(number|bigint)} zip64.size - uncompressed file size
  * @param {(number|bigint)} zip64.compressedSize - compressed file size
  * @param {(number|bigint)} zip64.offset - file header offset
  * @param {(number|bigint)} zip64.diskNumStart - disk number where file started
  */
-ExtraHeader.prototype.SetZip64 = function (zip64) {
+ExtraHeader.prototype.SetZip64 = function (centralHeader) {
     let length = 0;
     const big = (val) => (typeof val === "bigint" ? val : BigInt(val || 0));
-    if ("size" in zip64) length = ID_ZIP64_OFFSETS.COMPRESSED;
-    if ("compressedSize" in zip64) length = ID_ZIP64_OFFSETS.OFFSET;
-    if ("offset" in zip64) length = ID_ZIP64_OFFSETS.DISKNUM;
-    if ("diskNumStart" in zip64) length = ID_ZIP64_OFFSETS._len;
+
+    if ("size" in centralHeader) length = ID_ZIP64_OFFSETS.COMPRESSED;
+    if ("compressedSize" in centralHeader) length = ID_ZIP64_OFFSETS.OFFSET;
+    if ("offset" in centralHeader) length = ID_ZIP64_OFFSETS.DISKNUM;
+    if ("diskNumStart" in centralHeader) length = ID_ZIP64_OFFSETS._len;
 
     if (length > 0) {
         const result = Buffer.alloc(length);
 
         if (result.length >= ID_ZIP64_OFFSETS.COMPRESSED) {
-            result.writeBigInt64LE(big(zip64.size), ID_ZIP64_OFFSETS.UNCOMPRESSED);
+            result.writeBigInt64LE(big(centralHeader.size), ID_ZIP64_OFFSETS.UNCOMPRESSED);
+            if (centralHeader.size >= 0x4fffffff) centralHeader.size = Constants.EF_ZIP64_OR_32;
         }
         if (result.length >= ID_ZIP64_OFFSETS.OFFSET) {
-            result.writeBigInt64LE(big(zip64.compressedSize), ID_ZIP64_OFFSETS.COMPRESSED);
+            result.writeBigInt64LE(big(centralHeader.compressedSize), ID_ZIP64_OFFSETS.COMPRESSED);
+            if (centralHeader.compressedSize >= 0x4fffffff) centralHeader.compressedSize = Constants.EF_ZIP64_OR_32;
         }
         if (result.length >= ID_ZIP64_OFFSETS.DISKNUM) {
-            result.writeBigInt64LE(big(zip64.offset), ID_ZIP64_OFFSETS.OFFSET);
+            result.writeBigInt64LE(big(centralHeader.offset), ID_ZIP64_OFFSETS.OFFSET);
+            if (centralHeader.offset >= 0x4fffffff) centralHeader.offset = Constants.EF_ZIP64_OR_32;
         }
         if (result.length >= ID_ZIP64_OFFSETS._len) {
-            result.writeBigInt64LE(big(zip64.diskNumStart), ID_ZIP64_OFFSETS.DISKNUM);
+            result.writeBigInt64LE(big(centralHeader.diskNumStart), ID_ZIP64_OFFSETS.DISKNUM);
+            if (centralHeader.diskNumStart >= 0x4fff) centralHeader.diskNumStart = Constants.EF_ZIP64_OR_32;
         }
 
         this.set(Constants.ID_ZIP64, result);
@@ -151,4 +171,4 @@ ExtraHeader.prototype.SetZip64 = function (zip64) {
     }
 };
 
-module.export = ExtraHeader;
+module.exports = { ExtraHeader };
